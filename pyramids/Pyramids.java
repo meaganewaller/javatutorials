835b564619c5fe7b64fa23426437f93ed23ac98f
Meagan Waller <meaganewaller@gmail.com>
2013-10-29 14:02:12 -0400

    Pyramids

---
+++ pyramids/Pyramids.java
@@ -0,0 +1,11 @@
+public class Pyramids {
+
+  public static void main(String args[]) {
+    for (int x = 1; x < 10; x++) {
+      for (int y = 0; y < x; y++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
